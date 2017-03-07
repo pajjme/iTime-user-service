96f0fad96c91c370f1a72c829435c78fc93dd5c9
David Skeppstedt <david.skeppstedt@gmail.com>
2017-03-07 21:37:55 +0100

    Remove debug prints

--- google.py
+++ google.py
@@ -10,7 +10,6 @@ class Google:
     def generate_oauth(self,auth_code):
         oauth = client.credentials_from_clientsecrets_and_code(self.secret_file,['profile', 'email'],auth_code)
         self.credentials = oauth
-        self.print_id_token()
     
     def get_access_token(self):
         return self.credentials.access_token
@@ -24,5 +23,3 @@ class Google:
     def get_id(self):
         return self.credentials.id_token['sub']
 
-    def print_id_token(self):
-        print(self.credentials.id_token)
