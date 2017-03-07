9b5eafb44b3b252acde088fca87ba594bbaf389e
David Skeppstedt <david.skeppstedt@gmail.com>
2017-03-07 20:48:45 +0100

    Update main using google and controller objects

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from database import Database
 from account import Account
 from account_db_mapper import AccountDatabaseMapper
+from controller import Controller
+from google import Google
 import os
 
 def main():
+    
     #fetch database credentials from env variables
     db_name = os.environ["ITIME_DB"]
     db_user = os.environ["ITIME_DB_USER"]
     db_password = os.environ["ITIME_DB_PASSWORD"]
+
     db = Database(db_name,db_user,db_password)
     db.connect()
 
-    account_db_mapper = AccountDatabaseMapper(db)
+    #fetch google client secret file path
+    google_api_file = os.environ["ITIME_GOOGLE_API_FILE"]
+    google = Google(google_api_file)
 
+    #Temporary asking for auth code via stdin
+    auth_code = input("auth code\n")
 
-    account = Account(0,"herpa@derpa.com","123","30")
+    controller = Controller(db,google)
+    controller.new_account(auth_code)
 
-    account_db_mapper.save(account)
     print("Exiting...")
 
 
