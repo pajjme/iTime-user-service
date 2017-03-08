34afa910b095692d08cbb50b7bcf41c56fc33425
David Skeppstedt <david.skeppstedt@gmail.com>
2017-03-08 20:28:22 +0100

    Change some variables names to follow naming convention

--- controller.py
+++ controller.py
@@ -5,23 +5,24 @@ class Controller:
     def __init__(self,database,google):
        self.database = database
        self.google = google
-       self.acc_db_mapper = AccountDatabaseMapper(database)
+       self.account_db_mapper = AccountDatabaseMapper(database)
 
 
     def new_account(self,auth_code):
-        #call google object to get new tokens
+        #call google to get new tokens
         self.google.generate_oauth(auth_code)
 
+        access_token  = self.google.get_access_token()
+        refresh_token = self.google.get_refresh_token()
 
-        access_tok  = self.google.get_access_token()
-        refresh_tok = self.google.get_refresh_token()
         email = self.google.get_email()
         id = self.google.get_id()
-        #create a new account, supplying email + tokens
+
+        #Create a new account
         account = Account(id,
                         email,
-                        access_tok,
-                        refresh_tok)
+                        access_token,
+                        refresh_token)
 
-        #store a account inject the database
-        self.acc_db_mapper.save(account)
+        #Store account 
+        self.account_db_mapper.save(account)
