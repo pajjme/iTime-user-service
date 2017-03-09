7180938518e87c6048f92112f70409a01cae0dda
David Skeppstedt <david.skeppstedt@gmail.com>
2017-03-09 20:01:24 +0100

    Add test and implementation for json formatting to account model

--- account.py
+++ account.py
@@ -1,3 +1,5 @@
+import json
+
 class Account:
 
     def __init__(self,id,email,access_token,refresh_token):
@@ -6,4 +8,7 @@ class Account:
         self.access_token = access_token
         self.refresh_token = refresh_token
     
+    def get_json(self):
+        dump = json.dumps(self.__dict__,sort_keys=True, indent=4)
+        return dump
 
