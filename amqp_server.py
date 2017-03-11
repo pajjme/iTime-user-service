8ad31539092ebbcec62390e821983080cee97945
David Skeppstedt <david.skeppstedt@gmail.com>
2017-03-11 23:27:01 +0100

    Fix typos in Amqp server class

--- amqp_server.py
+++ amqp_server.py
@@ -7,10 +7,10 @@ class AmqpServer:
         self.procedure = procedure
 
         self.connection = pika.BlockingConnection(
-                pika.ConnectionParameters(host=server)
+                pika.ConnectionParameters(host=server))
 
         self.channel = self.connection.channel()
-        self.channel_declare(queue = request_queue)
+        self.channel.queue_declare(queue = request_queue)
         
         self.channel.basic_qos(prefetch_count=1)
         self.channel.basic_consume(self.on_request,queue=request_queue)
@@ -23,15 +23,16 @@ class AmqpServer:
     #Callback method that will be called on eache RPC request
 
     def on_request(self,channel,method,properties,body):
+        result = self.procedure(body)
 
-        #TODO call to controller here!
-        self.procedure(body)
-       
-        channel.baisc_publish(exchange='',
+        if result == None:
+             result = "{msg:empty}"
+         
+        channel.basic_publish(exchange='',
             routing_key= properties.reply_to,
             properties=pika.BasicProperties(
                 correlation_id = properties.correlation_id),
-            body = "Hello World!"
+            body = str(result) 
             )
 
         channel.basic_ack(delivery_tag = method.delivery_tag)
